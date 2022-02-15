892a975925a02a32267857b5b8442756f17b8134
MANTENN <nzrbeats@gmail.com>
2022-02-15 01:30:27 -0800

    removed on client entry to lazy load the module

--- src/onClientEntry.js
+++ src/onClientEntry.js
@@ -1,8 +1,5 @@
-import Modal from 'react-modal'
 
 const onClientEntry = (_args, opts = {}) => {
-  const { appElement = `#___gatsby` } = opts
-  Modal.setAppElement(appElement)
 }
 
 export default onClientEntry
